fc08d37defab8c40de816fe5e9b05f7924fa15bd
Rafael Madakis <rafaelmadakis@gmail.com>
2020-11-02 12:46:23 -0300

    Layout, AdapterHolder(VH)

---
+++ app/src/main/java/com/example/recyclerview/activity/adapter/Adapter.java
@@ -0,0 +1,41 @@
+package com.example.recyclerview.activity.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+/*Created by Rafael Madakis */
+public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    //View Holder (VH)
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView titulo;
+        TextView ano;
+        TextView genero;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
